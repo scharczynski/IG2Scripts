d2b25f524689bab78bda764dddb43669ab3a0d0d
scharczynski <scharcz@brandeis.edu>
2017-04-19 14:58:44 -0400

    linux changes

--- pexpect_scripts/preconnect_slave.py
+++ pexpect_scripts/preconnect_slave.py
@@ -9,11 +9,17 @@ def test(proc):
     if output == 2:
         cycle = pexpect.spawn('curl "http://admin:1234@192.168.100.36/outlet?2=OFF"')
 
+    else:
+        print "device appears to not be connected"
+        return False
+
     output = proc.expect(['Announce: RPC: Timed out', 'Announce\(\) fail', 'Device.*in error', pexpect.TIMEOUT], timeout=10)
 
     if output == 0 or 1:
         cycle = pexpect.spawn('curl "http://admin:1234@192.168.100.36/outlet?2=ON"')
-
+    else:
+        print "device time out not detected"
+        return False
     output = proc.expect([pexpect.TIMEOUT, pexpect.EOF, 'Announce\(\) success'], timeout=10)
 
     if output == 2:
@@ -21,4 +27,5 @@ def test(proc):
         return True
     else:
         print proc.before, proc.after
+        print "device did not reconnect"
         return False
--- pexpect_scripts/psamename.py
+++ pexpect_scripts/psamename.py
@@ -6,19 +6,14 @@ import pexpect
 
 
 def prepare(proc):
-    #(output, err) = proc.communicate()
+
     proc.expect('\[EXCEPTION[^:]*exists')
-    #proc.expect(pexpect.EOF)
+
     error = proc.after
     before = proc.before
-    #print "before " + before
-    print " after "+ error
-    #for line in output.splitlines():
-    #    if "EXCEPTION" in line:
-    #        error = line
-    #if not line:
-    #    print "there is no exception"
-    #    return "0"
+ 
+    #print " after "+ error
+
     expr = re.compile('\d{2}.\d{2}.\d{4} \d{2}:\d{2}:\d{2}:\d{3}')
     clean = re.sub(expr, '', error).lstrip()
     return clean
